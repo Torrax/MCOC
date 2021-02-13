86cfca56dd87f0e641215417bfc0f6a884882791
Torrax <Carson.morris2@gmail.com>
2021-02-13 13:52:14 -0500

    Update itemTest.lua

--- test/itemTest.lua
+++ test/itemTest.lua
@@ -7,14 +7,14 @@ local bankChest = sides.east                               -- sides.up, sides.do
 
 local counted = 0
 local searchfor = "1,000G"
-local winAmount = 22
+local winAmount = 4
 
 local i = 0
-for i = 1, (ct.getInventorySize(storageside)) do
-    local item = ct.getStackInSlot(storageside, i)
+for i = 1, (component.transposer.getInventorySize(bankChest)) do
+    local item = component.transposer.getStackInSlot(bankChest, i)
 
     if item then
-        if string.find(item.name .. "^" .. item.label .. "^", searchfor) then                   --string.match(component.transposer.getStackInSlot(sides.west,1).label, searchfor)
+        if string.match(component.transposer.getStackInSlot(bankChest,1).label, searchfor) then
             print(i .. ": " .. item.name .. " - " .. item.label .. " (" .. item.size .. ")")
             counted = counted + 1
             slot = i
@@ -22,11 +22,11 @@ for i = 1, (ct.getInventorySize(storageside)) do
     end
 end
 
-if counted == 1 then                                          -- If Chips were found
-    ct.transferItem(outputChest, bankChest, winAmount, slot)  -- Transfer Chips to Slot (Variable)
+if counted == 1 then                                                               -- If Chips were found
+    component.transposer.transferItem(bankChest, outputChest, winAmount, 1)        -- Transfer Chips to Slot (Variable)
 end
 
--- Transfer EVERY ITEM back to other chest
---  for i = ct.getInventorySize(chestside), 1, -1 do
---      ct.transferItem(bankChest, outputChest, 64, i)
+                                                                  -- Transfer EVERY ITEM back to other chest
+--  for i = ct.getInventorySize(bankChest), 1, -1 do
+--      ct.transferItem(outputChest, bankChest, 64, i)
 --  end
