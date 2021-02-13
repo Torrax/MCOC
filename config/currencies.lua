3c9be07cc49e4fa4c5303fa11a63a76b226fa381
Torrax <Carson.morris2@gmail.com>
2021-02-13 14:25:53 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -10,8 +10,7 @@ local function addCurrency(name, id, dmg, model, color, max)
     })
 end
 
-addCurrency("Chips",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
-addCurrency("Glowstone",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
-addCurrency("Nothing",        nil,                        nil, nil,     0xE5E5E5, nil)
+addCurrency("Chips",           "coins.coin",               0,   'INGOT', 0x85BB65, nil)
+addCurrency("Nothing",         nil,                        nil, nil,     0xE5E5E5, nil)
 
 return currencies
