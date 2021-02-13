bd4cb09fd0e32069e9b455455c2b03f6c4db1978
Torrax <Carson.morris2@gmail.com>
2021-02-12 20:20:53 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,8 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/Torrax/MCOC/main"
+local REPOSITOTY = "https://raw.githubusercontent.com/Torrax/MCOC/master"
 
 local shell = require("shell")
 shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
 shell.execute("wget -fq " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
+shell.execute("wget -fq " .. REPOSITOTY .. "/gameIns.lua /home/gameIns.lua")
 shell.execute("edit /lib/settings.lua")
