1adc219d1a2fa9e4175cfedc5456ba0338ec05ea
Torrax <Carson.morris2@gmail.com>
2021-02-14 02:48:47 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -1,16 +1,17 @@
 local currencies = {}
 
-local function addCurrency(name, id, dmg, model, color, max)
+local function addCurrency(name, id, lbl, dmg, model, color, max)
     table.insert(currencies, {
         name = name,
         id = id,
+        lbl = lbl
         dmg = dmg,
         model = model,
         color = color,
     })
 end
 
-addCurrency("Nothing",                nil,                 nil,    nil,   0xE5E5E5, nil)
-addCurrency("Chips",           "coins.coin",               0,   'INGOT', 0x85BB65, nil)
+addCurrency("Nothing",             nil,             nil,         nil,    nil,   0xE5E5E5, nil)
+addCurrency("Chips",           "coins.coin",      "1,000G",       0,   'INGOT', 0x85BB65, nil)
 
 return currencies
