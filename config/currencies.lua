eb6cfccecd2b22718728b94645169fd4d378216f
Torrax <Carson.morris2@gmail.com>
2021-02-14 23:58:57 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -14,9 +14,9 @@ end
 addCurrency("Nothing",                  nil,             nil,        nil,    nil,    0xE5E5E5,   nil)
 addCurrency("10 ©",                "coins.coin",        "10G",        0,   'COIN',   0xFDFD2E,   nil)
 addCurrency("100 ©",                "coins.coin",       "100G",       0,   'COIN',   0xFF0000,   nil)
-addCurrency("1,000 ©",              "coins.coin",       "1,000G",     0,   'COIN',   0x0000FF,   nil)
+addCurrency("1,000 ©",              "coins.coin",       "1,000G",     0,   'COIN',   0x00BCFF,   nil)
 addCurrency("10,000 ©",             "coins.coin",      "10,000G",     0,   'COIN',   0x85BB65,   nil)
 addCurrency("100,000 ©",            "coins.coin",      "100,000G",    0,   'COIN',   0xFF9900,   nil)
-addCurrency("1,000,000 ©",          "coins.coin",     "1,000,000G",   0,   'COIN',   0xFD8FFD,   nil)
+addCurrency("1,000,000 ©",          "coins.coin",     "1,000,000G",   0,   'COIN',   0x444444,   nil)
 
 return currencies
