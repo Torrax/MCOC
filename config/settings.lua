1e778e8dfef53cd673187ced096f2b29040a00fb
Torrax <Carson.morris2@gmail.com>
2021-02-16 18:51:19 -0500

    Update settings.lua

--- config/settings.lua
+++ config/settings.lua
@@ -8,6 +8,6 @@ settings.ADMINS = { "Torrax" }
 -- CHEST / PIM / ...
 settings.PAYMENT_METHOD = "CHEST"
 settings.CONTAINER_PAY = 4   -- BOTTOM = 0     TOP = 1    NORTH = 2   SOUTH = 3  WEST = 4   EAST = 5        
-settings.CONTAINER_GAIN = 5  -- BOTTOM = 0     TOP = 1    NORTH = 2   SOUTH = 3  WEST = 4   EAST = 5
+settings.CONTAINER_GAIN = 0  -- BOTTOM = 0     TOP = 1    NORTH = 2   SOUTH = 3  WEST = 4   EAST = 5
 
 return settings;
