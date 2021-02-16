f101d0265fc7634332996c1a5980d6850e69b72e
Torrax <Carson.morris2@gmail.com>
2021-02-16 04:49:17 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -11,12 +11,12 @@ local function addCurrency(name, id, lbl, dmg, model, color, max)
     })
 end
 
-addCurrency("Nothing",                  nil,             nil,        nil,    nil,    0xE5E5E5,   nil)
-addCurrency("10 ©",                "coins.coin",        "10G",        0,   'COIN',   0xFDFD2E,   nil)
-addCurrency("100 ©",                "coins.coin",       "100G",       0,   'COIN',   0xFF0000,   nil)
-addCurrency("1,000 ©",              "coins.coin",       "1,000G",     0,   'COIN',   0x6FA8DC,   nil)
-addCurrency("10,000 ©",             "coins.coin",      "10,000G",     0,   'COIN',   0x85BB65,   nil)
-addCurrency("100,000 ©",            "coins.coin",      "100,000G",    0,   'COIN',   0xFF9900,   nil)
-addCurrency("1,000,000 ©",          "coins.coin",     "1,000,000G",   0,   'COIN',   0x444444,   nil)
+addCurrency("Nothing",                     nil,                 nil,          nil,    nil,    0xE5E5E5,   nil)
+addCurrency("10 ©",                 "pokerchipz.chip1",      "10 Chip",        0,   'COIN',   0xFDFD2E,   nil)
+addCurrency("100 ©",                "pokerchipz.chip2",     "100 Chip",        0,   'COIN',   0xFF0000,   nil)
+addCurrency("1,000 ©",              "pokerchipz.chip3",     "1,000 Chip",      0,   'COIN',   0x6FA8DC,   nil)
+addCurrency("10,000 ©",             "pokerchipz.chip4",    "10,000 Chip",      0,   'COIN',   0x85BB65,   nil)
+addCurrency("100,000 ©",            "pokerchipz.chip5",    "100,000 Chip",     0,   'COIN',   0xFF9900,   nil)
+addCurrency("1,000,000 ©",          "pokerchipz.chip6",   "1,000,000 Chip",    0,   'COIN',   0x444444,   nil)
 
 return currencies
