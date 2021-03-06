d3d068bbdb9e3c7efc2260810d36846675c0eae8
Torrax <Carson.morris2@gmail.com>
2021-03-06 02:27:56 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -11,8 +11,8 @@ local function addCurrency(name, id, lbl, dmg, model, color, max)
     })
 end
 
-addCurrency("Nothing",                     nil,                 nil,          nil,    nil,    0xE5E5E5,   nil)
-addCurrency("10 ©",                 "pokerchipz.chip1",      "10 Chip",        0,   'COIN',   0xFDFD2E,   nil)
 addCurrency("100 ©",                "pokerchipz.chip2",     "100 Chip",        0,   'COIN',   0xFF0000,   nil)
+addCurrency("1,000 ©",              "pokerchipz.chip3",     "1,000 Chip",      0,   'COIN',   0x6FA8DC,   nil)
+addCurrency("10,000 ©",             "pokerchipz.chip4",    "10,000 Chip",      0,   'COIN',   0x85BB65,   nil)
 
 return currencies
