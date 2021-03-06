7e281b7911f76fe12379d7a1829d594f4d6c4179
Torrax <Carson.morris2@gmail.com>
2021-03-06 02:09:43 -0500

    Update currencies.lua

--- config/currencies.lua
+++ config/currencies.lua
@@ -14,9 +14,5 @@ end
 addCurrency("Nothing",                     nil,                 nil,          nil,    nil,    0xE5E5E5,   nil)
 addCurrency("10 ©",                 "pokerchipz.chip1",      "10 Chip",        0,   'COIN',   0xFDFD2E,   nil)
 addCurrency("100 ©",                "pokerchipz.chip2",     "100 Chip",        0,   'COIN',   0xFF0000,   nil)
-addCurrency("1,000 ©",              "pokerchipz.chip3",     "1,000 Chip",      0,   'COIN',   0x6FA8DC,   nil)
-addCurrency("10,000 ©",             "pokerchipz.chip4",    "10,000 Chip",      0,   'COIN',   0x85BB65,   nil)
-addCurrency("100,000 ©",            "pokerchipz.chip5",    "100,000 Chip",     0,   'COIN',   0xFF9900,   nil)
-addCurrency("1,000,000 ©",          "pokerchipz.chip6",   "1,000,000 Chip",    0,   'COIN',   0x444444,   nil)
 
 return currencies
